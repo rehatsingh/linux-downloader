e008cbbbad8deceb97a36918380fa2e0cfb5117d
rehat <rehatsingh2007@gmail.com>
2023-08-06 16:54:34 +0530

    fixed some spelling errors in install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ HOME=$(grep 1000 /etc/passwd | cut -d ':' -f 6)
 
 echo "getting version info..."
 #ver=$(wget --output-document - -q https://www.kernel.org/ | grep -A 1 latest_link | grep http | cut -d'"' -f2 | cut -d '.' -f 4-6 | cut -c 9-13)
-ver=$(cat $HOME/linux/ver/downloaded)
+ver=$(cat $HOME/build/linux/ver/downloaded)
 echo "linux $ver"
 sleep 1
 
 echo "coppying kernel to boot directory..."
-cp $HONE/build/linux/linux-$ver/arch/x86_64/boot/bzImage /boot/vmlinuz-$USER
+cp $HOME/build/linux/linux-$ver/arch/x86_64/boot/bzImage /boot/vmlinuz-$USER
 echo "coppied"
 sleep 1
 
@@ -26,4 +26,6 @@ sleep 1
 
 echo "$ver" > $HOME/build/linux/ver/installed
 
-echo -e "Reboot to automaticly use the newly compiled and installed kernell.\nAnd pat yourself on the back.\nI am proud of you, owner"
+echo "Reboot to automaticly use the newly compiled and installed kernell"
+echo "And pat yourself on the back"
+echo "I am proud of you, owner."
